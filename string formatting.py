a117f04082ad3dafde76017804cfdc69de7ee93e
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-07 20:58:50 +0530

    Add files via upload

---
+++ string formatting.py
@@ -0,0 +1,11 @@
+#PADDING
+print("***{msg}***".format(msg="WELCOME"))
+# O/P - ***WELCOME***
+print("***{msg:15}***".format(msg="WELCOME"))
+#O/P - ***WELCOME        *** add space end of the welcome
+print("***{msg:>15}***".format(msg="WELCOME"))
+#O/P - ***        WELCOME***
+print("***{msg:^15}***".format(msg="WELCOME"))
+#O/P -***    WELCOME    ***
+print("***{:^15}***".format("WELCOME"))
+#O/P -***    WELCOME    ***
